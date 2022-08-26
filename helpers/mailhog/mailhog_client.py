4454859757c28df2741be4915470cde124bbb9d7
ValeriyMenshikov <team095hunter@gmail.com>
2022-08-26 10:13:55 +0300

    Создали класс помошник для работы с почтовым сервером

---
+++ helpers/mailhog/mailhog_client.py
@@ -0,0 +1,25 @@
+import json
+import pprint
+
+import requests
+
+
+class MailHogClient:
+    def __init__(self, host='http://localhost:5025'):
+        self.host = host
+
+    def get_all_email(self, limit=50):
+        params = {
+            'limit': str(limit),
+        }
+
+        response = requests.get(
+            url=f'{self.host}/api/v2/messages',
+            params=params
+        )
+        return response
+
+    def get_token_from_last_email(self):
+        emails = self.get_all_email(limit=1)
+        token = json.loads(emails.json()['items'][0]['Content']['Body'])['ConfirmationLinkUrl'].split('/')[-1]
+        return token
